c0a50f88e784a600543e82a42f43c19f559bf5ce
aritradey-CS <81703791+aritradey-CS@users.noreply.github.com>
2023-05-21 21:34:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './components/Navbar';
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <h1>Aritra's Diary</h1>
+    </div>
+  )
+}
+
+export default Navbar
