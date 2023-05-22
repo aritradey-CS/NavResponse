9e69be70227de345bcb24e0b835102b9ce637de9
aritradey-CS <81703791+aritradey-CS@users.noreply.github.com>
2023-05-22 23:11:02 +0530

    Logical & Design updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,124 @@
-import './App.css';
-import './components/Navbar';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import React from 'react'
-
-export default function App() {
+const Home = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>
+          I am an engineering student in the Department of Computer Science, I
+          have completed my diploma course in 2019 and Graduation B.tech in
+          Computer Science following by 2022 now doing Master's. from my blog,
+          you can get informations for digital technologies which are may
+          included in your course or beyond!
+        </p>
+        <h1>Aritra's Diary Home Page</h1>
+      </section>
+    </>
+  );
+};
+
+const Pages = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>
+          How It Got Started Content creating and content writing is the job I
+          love to do!! I am an avid reader and writer. I have the ability to
+          read and analyze content and also love to create digital content.
+          Lastly, I worked for "LearnByExample" as a content writer. I have
+          completed my diploma engineering course in 2019 in Computer Science
+          and Technology. currently, I'm doing B.tech engineering in the same
+          department. Joining any organization will help me learn a lot. I'm
+          always ready to put my full effort to learn & growing my skills which
+          are beneficial for the job. I feel that the company can undoubtedly
+          help me build one. In turn, I can assure you of utmost dedication.
+          AestheticAritra is truly my own little passion project, gaining more
+          and more traction each day. I hope you enjoy browsing my site and all
+          of the unique content I have to offer. Take a look around; perhaps
+          you’ll discover what fuels you as well. Read on and enjoy!
+        </p>
+        <h1>Aritra's Diary About Page</h1>
+      </section>
+    </>
+  );
+};
+
+const Gallary = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>
+          How It Got Started Content creating and content writing is the job I
+          love to do!! I am an avid reader and writer. I have the ability to
+          read and analyze content and also love to create digital content.
+          Lastly, I worked for "LearnByExample" as a content writer. I have
+          completed my diploma engineering course in 2019 in Computer Science
+          and Technology. currently, I'm doing B.tech engineering in the same
+          department. Joining any organization will help me learn a lot. I'm
+          always ready to put my full effort to learn & growing my skills which
+          are beneficial for the job. I feel that the company can undoubtedly
+          help me build one. In turn, I can assure you of utmost dedication.
+          AestheticAritra is truly my own little passion project, gaining more
+          and more traction each day. I hope you enjoy browsing my site and all
+          of the unique content I have to offer. Take a look around; perhaps
+          you’ll discover what fuels you as well. Read on and enjoy!
+        </p>
+        <h1>Aritra's Diary Gallary Page</h1>
+      </section>
+    </>
+  );
+};
+const About = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>
+          How It Got Started Content creating and content writing is the job I
+          love to do!! I am an avid reader and writer. I have the ability to
+          read and analyze content and also love to create digital content.
+          Lastly, I worked for "LearnByExample" as a content writer. I have
+          completed my diploma engineering course in 2019 in Computer Science
+          and Technology. currently, I'm doing B.tech engineering in the same
+          department. Joining any organization will help me learn a lot. I'm
+          always ready to put my full effort to learn & growing my skills which
+          are beneficial for the job. I feel that the company can undoubtedly
+          help me build one. In turn, I can assure you of utmost dedication.
+          AestheticAritra is truly my own little passion project, gaining more
+          and more traction each day. I hope you enjoy browsing my site and all
+          of the unique content I have to offer. Take a look around; perhaps
+          you’ll discover what fuels you as well. Read on and enjoy!
+        </p>
+        <h1>Aritra's Diary About Page</h1>
+      </section>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/page">
+          <Pages />
+        </Route>
+        <Route exact path="/gallary">
+          <Gallary />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,99 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./navbar.css";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaYoutubeSquare,
+} from "react-icons/fa";
 
-function Navbar() {
+import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const [showMediaicons, setShowMediaIcons] = useState(false);
   return (
-    <div>
-      <h1>Aritra's Diary</h1>
-    </div>
-  )
-}
+    <>
+      <nav className="main-nav">
+        {/* first logo part */}
 
-export default Navbar
+        <div className="logo">
+          <h2>
+            <span>A</span>ritra's
+            <br />
+            <span>D</span>iary
+          </h2>
+        </div>
+
+        {/* second menu part */}
+
+        <div
+          className={
+            showMediaicons ? "menu-link mobile-menu-link" : "menu-link"
+          }
+        >
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/pages">Pages</NavLink>
+            </li>
+            <li>
+              <NavLink to="/gallary">Gallary</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+          </ul>
+        </div>
+
+        {/* Social media links */}
+
+        <div className="social-media">
+          <ul className="social-media-desktop">
+            <li>
+              <a
+                href="https://www.facebook.com/aritra.mukhapadhyaydey/"
+                target="-aritra"
+              >
+                <FaFacebookSquare className="facebook" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/aritra_dey_cs/"
+                target="-aritra"
+              >
+                <FaInstagramSquare className="instagram" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/@aritradey1411/videos"
+                target="-aritra"
+              >
+                <FaYoutubeSquare className="youtube" />
+              </a>
+            </li>
+          </ul>
+
+          {/* hambuerger menu start */}
+
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaicons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
+        </div>
+      </nav>
+
+      {/* Hero section */}
+      {/* <section className="hero-section">
+        <p>Welcome to</p>
+        <h1>Aritra's Diary</h1>
+      </section> */}
+    </>
+  );
+};
+
+export default Navbar;
